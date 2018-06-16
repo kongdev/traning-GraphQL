7dad5acad870680877fdd0ecd980bb9110fbaf27
kongdev <nopaket_kong@hotmail.com>
2018-06-16 12:55:37 +0700

    ok

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,43 +1,5 @@
 const { Post, User } = require("../models");
 
-//const dataLoader = require("dataloader")
-/*
-const userLoader = new dataLoader(async(keys)=>{
-	
-	const rows = await User.find({_id : {$in : keys}})
-	
-	const results = keys.map((key)=>{
-		const matchRow = rows.find((row)=>{
-			return `${row._id}` === `${key}`
-		})
-		return matchRow
-	})
-
-	return results
-	
-},{cacheKeyFn : (key)=>{
-	return `${key}`
-}})
-*/
-/*
-const postLoaderIdLoader = new dataLoader(async(userIds)=>{
-	
-	const rows = await Post.find({authorId : {$in : userIds}})
-
-	const results = userIds.map((userId)=>{
-		const matchRow = rows.filter((row)=>{
-			return `${row.authorId}` === `${userId}`
-		})
-		return matchRow
-	})
-
-	return results
-	
-},{cacheKeyFn : (key)=>{
-	return `${key}`
-}})
-*/
-
 module.exports = {
 	Tag: {
 		posts: async tag => {
